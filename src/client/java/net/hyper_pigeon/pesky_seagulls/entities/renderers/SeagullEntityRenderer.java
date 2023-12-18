6a3c13ac32fad46e3cf4b2045c43cb6e5d381c07
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2023-12-18 14:10:16 -0500

    Added parrot model as placeholder

--- src/client/java/net/hyper_pigeon/pesky_seagulls/entities/renderers/SeagullEntityRenderer.java
+++ src/client/java/net/hyper_pigeon/pesky_seagulls/entities/renderers/SeagullEntityRenderer.java
@@ -8,14 +8,18 @@ import net.minecraft.client.render.entity.model.EntityModelLayers;
 import net.minecraft.util.Identifier;
 
 public class SeagullEntityRenderer extends MobEntityRenderer<SeagullEntity, SeagullEntityModel<SeagullEntity>> {
+
+    private static final Identifier BLUE_TEXTURE = new Identifier("textures/entity/parrot/parrot_blue.png");
+
     public SeagullEntityRenderer(EntityRendererFactory.Context context) {
-        //Set EntityModelLayer to Parrot as a placeholder
+        //Use parrot model as placeholder
         super(context, new SeagullEntityModel(context.getPart(EntityModelLayers.PARROT)), 0.3F);
     }
 
 
     @Override
+    //using blue parrot texture as placeholder
     public Identifier getTexture(SeagullEntity entity) {
-        return null;
+        return BLUE_TEXTURE;
     }
 }
