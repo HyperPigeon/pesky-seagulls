8940b5405c0d8b8a65bb1c046acd96251810b819
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2023-12-17 22:14:21 -0500

    Initial setup

---
+++ src/main/java/net/hyper_pigeon/pesky_seagulls/client/PeskySeagullsClient.java
@@ -0,0 +1,10 @@
+package net.hyper_pigeon.pesky_seagulls.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PeskySeagullsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
