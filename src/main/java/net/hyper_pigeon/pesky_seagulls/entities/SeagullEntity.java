3a93830b700c7d2f0fc25b7f1a6cf89dcea88564
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2023-12-24 13:08:05 -0500

    Re-added gravity to seagulls when attacking.

--- src/main/java/net/hyper_pigeon/pesky_seagulls/entities/SeagullEntity.java
+++ src/main/java/net/hyper_pigeon/pesky_seagulls/entities/SeagullEntity.java
@@ -170,9 +170,6 @@ public class SeagullEntity extends AnimalEntity implements SmartBrainOwner<Seagu
     @Override
     protected void mobTick() {
         tickBrain(this);
-        if(BrainUtils.getTargetOfEntity(this) != null){
-            this.setNoGravity(true);
-        }
     }
 
 
