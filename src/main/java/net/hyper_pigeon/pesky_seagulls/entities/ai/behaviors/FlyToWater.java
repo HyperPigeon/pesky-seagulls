3a9cf643ce6ff275623e6cf4d1e3e9cb0dd7a208
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2023-12-31 13:50:26 -0500

    Reverted to previous implementation

--- src/main/java/net/hyper_pigeon/pesky_seagulls/entities/ai/behaviors/FlyToWater.java
+++ src/main/java/net/hyper_pigeon/pesky_seagulls/entities/ai/behaviors/FlyToWater.java
@@ -1,12 +1,13 @@
 package net.hyper_pigeon.pesky_seagulls.entities.ai.behaviors;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.ai.NoPenaltySolidTargeting;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
+import net.minecraft.world.BlockView;
 import net.tslat.smartbrainlib.api.core.behaviour.custom.path.SetRandomFlyingTarget;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,21 +15,21 @@ public class FlyToWater<E extends PathAwareEntity> extends SetRandomFlyingTarget
     @Nullable
     @Override
     protected Vec3d getTargetPos(E entity) {
-        Vec3d waterPos = findNearbyWater(entity);
+        BlockPos waterPos = locateClosestWater(entity.getWorld(), entity,  (int)(Math.ceil(this.radius.xzRadius())), (int)Math.ceil(this.radius.yRadius()));
         Vec3d entityFacing = entity.getRotationVec(0);
 
         if (waterPos != null)
-            return waterPos;
+            return new Vec3d(waterPos.getX(),waterPos.getY(),waterPos.getZ());
 
         return NoPenaltySolidTargeting.find(entity, (int)(Math.ceil(this.radius.xzRadius())), (int)Math.ceil(this.radius.yRadius()), this.verticalWeight.applyAsInt(entity), entityFacing.x, entityFacing.z, MathHelper.HALF_PI);
     }
 
-
     @Nullable
-    protected Vec3d findNearbyWater(E entity) {
-        final BlockPos pos = entity.getBlockPos();
-        final World level = entity.getWorld();
-
-        return !level.getBlockState(pos).getCollisionShape(level, pos).isEmpty() ? null : BlockPos.findClosest(entity.getBlockPos(), (int)this.radius.xzRadius(), (int)this.radius.yRadius(), checkPos -> level.getFluidState(checkPos).isIn(FluidTags.WATER)).map(Vec3d::ofBottomCenter).orElse(null);
+    protected BlockPos locateClosestWater(BlockView world, Entity entity, int rangeX , int rangeY) {
+        BlockPos blockPos = entity.getBlockPos();
+        if (!world.getBlockState(blockPos).getCollisionShape(world, blockPos).isEmpty()) {
+            return null;
+        }
+        return BlockPos.findClosest(entity.getBlockPos(), rangeX, rangeY, pos -> world.getFluidState((BlockPos)pos).isIn(FluidTags.WATER)).orElse(null);
     }
 }
